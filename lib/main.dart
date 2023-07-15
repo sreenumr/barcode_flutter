e33919801522006c28757a033a455b0b9dbf0cf5
sreenumr <sreenu959@gmail.com>
2023-07-15 19:42:22 +0100

    Browsed file name displayed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'widgets/pages/home.dart';
+import 'package:provider/provider.dart';
+import 'package:file_picker/file_picker.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +13,33 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+        home: const MyHomePage(
+          title: "Home",
+        ),
       ),
-      home: const MyHomePage(title: 'Home Page'),
     );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var selectedFile = "";
+
+  void pickFile() async {
+    final result = await FilePicker.platform.pickFiles(allowMultiple: false);
+    if (result == null) return;
+
+    selectedFile = result.files.first.name;
+    print(selectedFile);
+    // print(result.files.first.size);
+    // print(result.files.first.path);
+    notifyListeners();
   }
 }
--- lib/widgets/pages/home.dart
+++ lib/widgets/pages/home.dart
@@ -1,7 +1,6 @@
-import 'dart:math';
-
+import 'package:barcode_app/main.dart';
 import 'package:flutter/material.dart';
-import 'package:file_picker/file_picker.dart';
+import 'package:provider/provider.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -17,25 +16,9 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {});
   }
 
-  void _pickFile() async {
-    // opens storage to pick files and the picked file or files
-    // are assigned into result and if no file is chosen result is null.
-    // you can also toggle "allowMultiple" true or false depending on your need
-    final result = await FilePicker.platform.pickFiles(allowMultiple: false);
-
-    // if no file is picked
-    if (result == null) return;
-
-    print(result.files.first.name);
-    // we will log the name, size and path of the
-    // first picked file (if multiple are selected)
-    // print(result.files.first.name);
-    // print(result.files.first.size);
-    // print(result.files.first.path);
-  }
-
   @override
   Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -45,15 +28,15 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            ElevatedButton(onPressed: _pickFile, child: const Text("Browse"))
+            Text(appState.selectedFile),
+            ElevatedButton(
+                onPressed: () {
+                  appState.pickFile();
+                },
+                child: const Text("Browse"))
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
